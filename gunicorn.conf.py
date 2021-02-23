9eb68091cd2aa081739faaf2200919044e8197bd
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-02-23 11:48:55 +0700

    Update file

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+# gunicorn.config.py
 """
-Gunicorn with Uvicorn config to launch in Digital Ocean's App Platform.
+Gunicorn Uvicorn config to lauch in Digital Ocean's App Platform.
+Using their Flask template: https://github.com/digitalocean/sample-flask
 """
+
 bind = "0.0.0.0:8080"
-workers = 2
-# Uvicorn's Gunicorn worker class
+workers = 4
+# Using Uvicorn's Gunicorn worker class
 worker_class = "uvicorn.workers.UvicornWorker"
