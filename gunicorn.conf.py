9fa4621a5c9aff2b60c30240e77a89ba53f10ea6
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-02-23 11:56:47 +0700

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,6 +6,6 @@ Using their Flask template: https://github.com/digitalocean/sample-flask
 """
 
 bind = "0.0.0.0:8080"
-workers = 4
+workers = 1
 # Using Uvicorn's Gunicorn worker class
 worker_class = "uvicorn.workers.UvicornWorker"
